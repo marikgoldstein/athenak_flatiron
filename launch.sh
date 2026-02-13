0dd486e294af667460b93ab8eea08389776d0978
Mark Goldstein <mgoldstein@workergpu003.cm.cluster>
2026-02-13 02:27:29 -0500

    >

--- launch.sh
+++ launch.sh
@@ -6,7 +6,14 @@ DECK="/mnt/home/mgoldstein/athenak_flatiron/decks/mri2d.athinput.2048"
 OUT="/mnt/home/mgoldstein/ceph/athenak/feb13_res2048/"
 set -euo pipefail                                                                                                 
 module purge
-module load slurm cuda openmpi
+module load slurm cuda openmpi/cuda-4.1.8
+
 export LD_PRELOAD=${LD}
 export CEPHTWEAKS_LAZYIO=1
-eval "${EXE} -i ${DECK} -d ${OUT}"
+
+mpirun -np 4 \
+    --bind-to core \
+    --map-by ppr:4:node \
+    -x LD_PRELOAD \
+    -x CEPHTWEAKS_LAZYIO \
+    bash -c 'export CUDA_VISIBLE_DEVICES=${OMPI_COMM_WORLD_LOCAL_RANK}; '"${EXE}"' -i '"${DECK}"' -d '"${OUT}"
